d2797c8476fb22f688f57862f11203c11a948354
AxXel701 <al222310386@gmail.com>
2025-04-11 00:23:35 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import '@coreui/coreui/dist/css/coreui.min.css'
-import '@coreui/icons/css/all.min.css'
+import '@coreui/icons/css/all.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
